fef4c90dd981293bca5d7e8d1f29df0aa5689181
Sacha DERET <sderet@e1r10p9.42.fr>
2018-02-22 18:15:54 +0100

    Version finale(?)

--- display_fun.c
+++ display_fun.c
@@ -6,11 +6,12 @@
 /*   By: sderet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 16:11:13 by sderet            #+#    #+#             */
-/*   Updated: 2018/02/20 18:18:58 by sderet           ###   ########.fr       */
+/*   Updated: 2018/02/22 17:45:39 by sderet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
+#include <stdlib.h>
 
 void	print_pixel(t_image *img, t_pos *pos, int *colo)
 {
@@ -27,21 +28,20 @@ void	print_pixel(t_image *img, t_pos *pos, int *colo)
 		((int)pos->y * img->line_len)] = (char)0;
 }
 
-void	print_pixelc(t_image *img, t_pos *pos, t_dposd *c, int slice)
+void	pre_ppc(int *colo, t_image *img, t_dposd *c, int mode)
 {
-	int colo[3];
 	int a;
 	int b;
-	int	y_start;
 
-	y_start = (WINDOW_Y - slice) / 2;
-	b = (c == 0 ? 0 : 122);
-	b = (c != 0 && c->cot == 1 ? 255 : b + 0);
-	a = -1;
-	while (++a < 3)
-		colo[a] = b;
-	a = -1;
-	if (c != 0)
+	if (mode == 1)
+	{
+		b = (c == 0 ? 0 : 122);
+		b = (c != 0 && c->cot == 1 ? 255 : b + 0);
+		a = -1;
+		while (++a < 3)
+			colo[a] = b;
+	}
+	else
 	{
 		colo[0] = (img->ang < 180 && c->cot == 1 ? 50 : colo[0] + 0);
 		colo[1] = (img->ang < 180 && c->cot == 1 ? 0 : colo[1] + 0);
@@ -49,6 +49,21 @@ void	print_pixelc(t_image *img, t_pos *pos, t_dposd *c, int slice)
 				colo[1] + 0);
 		colo[2] = (img->ang < 270 && img->ang > 90 && c->cot == 0 ? 0 :
 				colo[2] + 0);
+	}
+}
+
+void	print_pixelc(t_image *img, t_pos *pos, t_dposd *c, int slice)
+{
+	int colo[3];
+	int a;
+	int	y_start;
+
+	pre_ppc(colo, img, c, 1);
+	y_start = (WINDOW_Y - slice) / 2;
+	a = -1;
+	if (c != 0)
+	{
+		pre_ppc(colo, img, c, 2);
 		if (((ABS((int)c->d.x) % BLOC_SIZE >= BLOC_SIZE - (BLOC_SIZE / 10)
 				|| ABS((int)c->d.x) % BLOC_SIZE <= (BLOC_SIZE / 10)) &&
 				c->cot == 1) || ((ABS((int)c->c.y) % BLOC_SIZE >= BLOC_SIZE
